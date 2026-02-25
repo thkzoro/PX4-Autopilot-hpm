18494c47a5e87a554b148e57a29c88af7566879c
Zhihong Chen <zhihong.chen@hpmicro.com>
2026-02-25 10:21:26 +0800

    hpmicro: add i2c last instance check
    Signed-off-by: Zhihong Chen <zhihong.chen@hpmicro.com>

--- boards/px4/fmu-v6xhpm-siyi/src/i2c.cpp
+++ boards/px4/fmu-v6xhpm-siyi/src/i2c.cpp
@@ -84,7 +84,7 @@ constexpr px4_i2c_bus_t px4_i2c_buses[I2C_BUS_MAX_BUS_ITEMS] = {
 	// initI2CBusExternal(0),
 	// initI2CBusExternal(1),
 	initI2CBusExternal(2),
-	// initI2CBusInternal(3),
+	initI2CBusInternal(-1),
 };
 
 
